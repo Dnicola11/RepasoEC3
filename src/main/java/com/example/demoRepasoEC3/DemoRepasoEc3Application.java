2f1a9c34a3ef2f1e07b7429151c42df19c4ac5c3
Dnicola11 <135287080+Dnicola11@users.noreply.github.com>
2023-07-24 21:24:05 +0000

    commit 1

---
+++ src/main/java/com/example/demoRepasoEC3/DemoRepasoEc3Application.java
@@ -0,0 +1,13 @@
+package com.example.demoRepasoEC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRepasoEc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRepasoEc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoRepasoEC3/DemoRepasoEc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoRepasoEC3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRepasoEc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
